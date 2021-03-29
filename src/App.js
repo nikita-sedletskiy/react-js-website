1243432b23ab8091318f6884c0f0d95e7ab75e12
nikita-sedletskiy <nikitasedletskiy@icloud.com>
2021-03-29 22:53:08 +0300

    feat: 4 pages (Home, About, Contacts and Blog) + routing

--- src/App.js
+++ src/App.js
@@ -8,11 +8,6 @@ function App() {
   return (
     <div>
       <Header/>
-      <h1>asd</h1>
-      <h1>asd</h1>
-      <h1>asd</h1>
-      <h1>asd</h1>
-
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,12 @@
-import { Button } from 'bootstrap'
 import React, { Component } from 'react'
-import { Container, FormControl, Nav, Navbar } from 'react-bootstrap'
+import { Container, FormControl, Nav, Navbar, Button, Form } from 'react-bootstrap'
 import logo from './logo192.png'
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
 
+import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Contacts from '../Pages/Contacts';
+import Blog from '../Pages/Blog';
 
 export default class Header extends Component {
     render() {
@@ -31,6 +35,14 @@ export default class Header extends Component {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contacts" component={Contacts} />
+                    <Route exact path="/blog" component={Blog} />
+                </Switch>
+            </Router>
             </>
         )
     }
